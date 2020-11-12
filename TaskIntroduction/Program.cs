5429c403089e35132eee3da6de995b0a9608e001
Maguin-barboza <magno_barboza@hotmail.com>
2020-11-12 17:07:31 -0300

    Add project files.

---
+++ TaskIntroduction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(() =>
+            {
+                for (int i=0; i<50; i++)
+                {
+                    Console.WriteLine("Task:" + i.ToString());
+                    Thread.Sleep(500);
+                }
+            });
+
+            task.Start();
+            Console.ReadKey();
+        }
+    }
+}
